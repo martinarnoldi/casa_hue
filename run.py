e8d4956891d9e96c5f00d5c3de16ca45d38de0d7
martinarnoldi <martin_arnoldi@hotmail.com>
2018-03-03 16:00:13 +0100

    sfd

---
+++ run.py
@@ -0,0 +1,29 @@
+import phue
+
+
+#!/usr/bin/python
+from Tkinter import *
+from phue import Bridge
+
+'''
+This example creates a slider that controls the
+brightness of the first 3 lights.
+'''
+
+b = Bridge("192.168.0.20") # Enter bridge IP here.
+
+#If running for the first time, press button on bridge and run with b.connect() uncommented
+b.connect()
+
+b.set_light([1,2,3], 'on', True)
+
+def sel(data):
+    b.set_light([1,2,3],{'bri':int(data), 'transitiontime': 1})
+
+root = Tk()
+scale = Scale( root, from_ = 254, to = 0, command= sel, length = 200 )
+scale.set(b.get_light(1,'bri'))
+scale.pack(anchor=CENTER)
+
+root.mainloop()
+
